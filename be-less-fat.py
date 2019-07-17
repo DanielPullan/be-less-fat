2b757dcbc4b140ec05aaadaf2aa6898362e5b282
Dan Pullan <dan@3264.uk>
2019-07-17 22:52:47 +0100

    Just discovered emoji support
    It's all downhill from here.

--- be-less-fat.py
+++ be-less-fat.py
@@ -2,36 +2,40 @@
 ## Weight loss data and visualisations
 ## 17/07/2019
 
-## We're all about the metric system here.
+## We're all about the metric system here. 🇪🇺
 ## Weight - kilograms (kg), height - metres (m), calories (kcal)
 
 import sys
 
 if sys.argv[1] == "help":
 	print("the order is height, weight, calories, age")
+	raise SystemExit
+elif sys.argv[1] == "🤘":
+	print("Metallica ruuuuuuules")
+	raise SystemExit
 else:
 	height = float(sys.argv[1])
 	weight = int(sys.argv[2])
 	daily_calorie_intake = int(sys.argv[3])
 	age = int(sys.argv[4])	
 
-	def bmiCalc(weight, height, daily_calorie_intake, age):
-		weight = weight
-		height = height
-		daily_calorie_intake = daily_calorie_intake
-		bmi = weight / (height**2)
-		return bmi
+def bmiCalc(weight, height, daily_calorie_intake, age):
+	weight = weight
+	height = height
+	daily_calorie_intake = daily_calorie_intake
+	bmi = weight / (height**2)
+	return bmi
 
-	bmi = bmiCalc(weight, height, daily_calorie_intake, age)
+bmi = bmiCalc(weight, height, daily_calorie_intake, age)
 
 
-	if bmi < 18.5:
-		print("You are very underweight")
-	elif bmi > 18.5 and bmi < 25:
-		print("You are a normal weight")
-	elif bmi > 25 and bmi < 35:
-		print("You are overweight")
-	elif bmi > 35:
-		print("You are very overweight.")
-	else: 
-		print("Error")
+if bmi < 18.5:
+	print("You are very underweight")
+elif bmi > 18.5 and bmi < 25:
+	print("You are a normal weight")
+elif bmi > 25 and bmi < 35:
+	print("You are overweight")
+elif bmi > 35:
+	print("You are very overweight.")
+else: 
+	print("Error")
